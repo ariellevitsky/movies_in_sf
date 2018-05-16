367660710801da18b35182a5bb5527373f6d87e4
Ariel Levitsky <ariel.levitsky@gmail.com>
2018-05-16 11:08:33 -0400

    clean up some ifs

--- main.js
+++ main.js
@@ -93,22 +93,18 @@ Vue.component('movie-browser', {
 			if(type == 'initialLoad' || type == 'loadMore') { //Avoid checking for sort/search parameters if it is the initial load or we are loading the next page
 				customParams = this.currentParams;
 			}
-			else { //Check if sorting is enabled
-				if(this.sortByYear && this.sortByTitle) {
-					customParams.params.$order = 'release_year,title';
-				}
-				else if(this.sortByYear || this.sortByTitle) {
-					if(this.sortByYear) {
-						customParams.params.$order = 'release_year'; 		
-					}
-					else if(this.sortByTitle) {
-						customParams.params.$order = 'title'; 	
-					}
-				}
-				else {
-					customParams = { params: { $order: ':id' } }
-				}
-			}
+			else if(this.sortByYear && this.sortByTitle) {
+                customParams.params.$order = 'release_year,title';
+            }
+            else if(this.sortByYear) {
+                customParams.params.$order = 'release_year';
+            }
+            else if(this.sortByTitle) {
+                customParams.params.$order = 'title'; 	
+            }
+            else {
+                customParams = { params: { $order: ':id' } }
+            }
 
 			if(this.searchValue && this.searchValue != '') { //Add a search query if one exists
 				customParams.params.$select = '*';
